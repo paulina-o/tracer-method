611e576c3eac7fbbe9006f8b14bb108196175483
Paulina Olejniczak <paulinaolejniczak@MacBook-Pro-Paulina-2.local>
2020-06-05 22:47:35 +0200

    Update setup.py file

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(name='tracer_method',
       version='1.0',
       description='Hydrological data analysis package',
       url='https://github.com/paulina-o/tracer_method',
-      packages=['tracer_method', 'tracer_method.core'],
+      packages=['tracer_method', 'tracer_method.core', 'tracer_method.core.config', 'tracer_method.core.curve_fitter',
+                'tracer_method.core.read_data', 'tracer_method.core.tritium'],
       zip_safe=False)
